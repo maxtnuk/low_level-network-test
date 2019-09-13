37ac066c385f1b37e91c12bca43e8cb59a07cf8f
maxtnuk <maxtnuk@gmail.com>
2019-09-14 01:28:51 +0900

    Add pci functions (allocate memory resource)

---
+++ src/pci.rs
@@ -0,0 +1,85 @@
+use std::error::Error;
+use std::fs::{self, File, OpenOptions};
+use std::io::{self, ErrorKind, Seek, SeekFrom, Write};
+use std::os::unix::prelude::AsRawFd;
+use std::ptr;
+
+use byteorder::{NativeEndian, ReadBytesExt, WriteBytesExt};
+
+//this mod functions for my laptop network pcie
+//pcie version pcie 3.0
+
+pub(in crate::pci) const CMD_REG_OFFSET: u64 = 4;
+pub(in crate::pci) const BUS_MS_ENABLE_BIT: u64 = 2;
+
+const ROOT_PCI_DVICE: &str = "/sys/bus/pci/devices/";
+
+type IOResult = Result<(), Box<dyn Error>>;
+
+pub fn add_pci_unbind(pci_addr: &str) -> IOResult {
+    let path = ROOT_PCI_DVICE.to_owned() + pci_addr + "/driver/unbind";
+
+    match OpenOptions::new().write(true).open(path) {
+        Ok(mut f) => {
+            f.write_all(pci_addr.as_bytes()).unwrap();
+            Ok(())
+        }
+        Err(ref e) if e.kind() == ErrorKind::NotFound => {
+            println!("there is no ubind file");
+            Ok(())
+        }
+        Err(ref e) if e.kind() == ErrorKind::Other => {
+            println!("other io problum occur");
+            Ok(())
+        }
+        Err(e) => Err(Box::new(e)),
+    }
+}
+
+pub fn enable_dma(pci_addr: &str) -> IOResult {
+    let path = ROOT_PCI_DVICE.to_owned() + pci_addr + "/config";
+    let mut f = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(path)
+        .unwrap();
+
+    assert_eq!(f.seek(SeekFrom::Start(CMD_REG_OFFSET))?, CMD_REG_OFFSET);
+    let mut dma = f.read_u16::<NativeEndian>()?;
+
+    dma |= 1 << BUS_MS_ENABLE_BIT;
+
+    assert_eq!(f.seek(SeekFrom::Start(CMD_REG_OFFSET))?, CMD_REG_OFFSET);
+    f.write_u16::<NativeEndian>(dma)?;
+
+    Ok(())
+}
+
+pub fn pci_map_resource(pci_addr: &str) -> Result<(*mut u8, usize), Box<dyn Error>> {
+    let path = ROOT_PCI_DVICE.to_owned() + pci_addr + "/resource0";
+    add_pci_unbind(pci_addr).unwrap();
+    enable_dma(pci_addr).unwrap();
+
+    let f = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(&path)
+        .unwrap();
+    let len = fs::metadata(path)?.len() as usize;
+
+    let ptr = unsafe {
+        libc::mmap(
+            ptr::null_mut(),
+            len,
+            libc::PROT_READ | libc::PROT_WRITE,
+            libc::MAP_SHARED,
+            f.as_raw_fd(),
+            0,
+        ) as *mut u8
+    };
+    if ptr.is_null() || len == 0 {
+        Err("pci mapping fail".into())
+    } else {
+        Ok((ptr, len))
+    }
+}
