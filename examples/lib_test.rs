947a23c45fa6e8c03f0c7b808efa6c8f5572acaa
maxtnuk <maxtnuk@gmail.com>
2019-09-14 23:25:09 +0900

    Add Example Case for Lib Test

---
+++ examples/lib_test.rs
@@ -0,0 +1,8 @@
+use lr_network::prelude::*;
+
+//Don't use this to test library
+//This example for test library inner functions
+fn main() {
+    check_kernel_version();
+    println!("Hello world");
+}
